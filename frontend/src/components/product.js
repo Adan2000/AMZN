55665e55c141929e3b94d1e20c5d3f7d9587dc27
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-21 15:32:34 -0700

    product.js

--- frontend/src/components/product.js
+++ frontend/src/components/product.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-export default function product() {
+export default function product(props) {
+    const { product } = props;
   return (
     <div key={product._id} className="card">
       <a href={`/product/${product._id}`}>
