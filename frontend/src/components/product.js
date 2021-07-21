659a96ed24446b2bbd8348ab0bfbd30e6e93b28a
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-21 15:42:58 -0700

    rating.js

--- frontend/src/components/product.js
+++ frontend/src/components/product.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Rating from "./rating";
 
 export default function product(props) {
-    const { product } = props;
+  const { product } = props;
   return (
     <div key={product._id} className="card">
       <a href={`/product/${product._id}`}>
@@ -11,6 +12,10 @@ export default function product(props) {
         <a href={`/product/${product._id}`}>
           <h2>{product.name}</h2>
         </a>
+        <Rating
+          rating={product.rating}
+          numReviews={product.numReviews}
+        ></Rating>
         <div className="price">${product.price}</div>
       </div>
     </div>
