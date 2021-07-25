5194c0842b08f8e0c01acbc1d111c9107846e57f
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-24 21:29:54 -0700

    react dom code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,36 @@
 import React from "react";
 import data from "./data";
 import Product from "./components/product";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            AMZ
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <Product key={product.id} product={product}></Product>
-            ))}
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              AMZ
+            </a>
           </div>
-        </div>
-      </main>
-      <footer className="row center">All right resevered.</footer>
-    </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <div>
+            <div className="row center">
+              {data.products.map((product) => (
+                <Product key={product.id} product={product}></Product>
+              ))}
+            </div>
+          </div>
+        </main>
+        <footer className="row center">All right resevered.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
