08c8a84e19c06c238e913c954435128a0ea2c7b4
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-26 20:22:50 -0700

    product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -17,6 +17,7 @@ export default function ProductScreen() {
               <li><h1>{product.name}</h1></li>
               <li><Rating rating={product.rating} alt={product.name}></Rating></li>
               <li>Price : ${profuct.price}</li>
+              <li>Description : <p>{product.description}</p></li>
             </ul>
           </div>
           <div className="col-1">
