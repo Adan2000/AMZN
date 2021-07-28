4308a31a84143c62b460f03ab0df80a534ab1af9
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-27 18:54:47 -0700

    product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,13 +2,13 @@ import React from "react";
 import data from "../data";
 
 export default function ProductScreen() {
-  const product = data.products.find(x => x._id === props.match.params.id);
+  // const product = data.products.find(x => x._id === props.match.params.id);
   if(!product){
     return <div>Product Not Found</div>
   }
   return (
     <div>
-        <div className="row">
+        <div className="row top">
           <div className="col-2">
             <img className="large" src={product.image} alt={product.name}></img>
           </div>
