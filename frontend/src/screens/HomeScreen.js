4256ee679ae766fa012fe4d3922d4cc150f1067f
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-29 21:49:32 -0700

    axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Product from '../components/Product';
 import data from '../data';
 
 export default function HomeScreen() {
+
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await axios.get('/api/products')
       setProducts(data)
     }
+    fetchData()
     return () => {
     }
-  }, [])
+  }, []);
+
   return (
     <div>
       <div className="row center">
