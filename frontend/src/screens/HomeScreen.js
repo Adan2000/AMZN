93e4f44d65f8c5585055ce9375add71168b5fdad
Adan Escamilla <adanescamilla580@gmail.com>
2021-07-31 22:05:07 -0700

    message box

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Product from "../components/Product";
+import LoadingBox from "../components/LoadingBox";
 
 export default function HomeScreen() {
   const [products, setProducts] = useState([]);
