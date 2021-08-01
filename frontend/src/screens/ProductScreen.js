7301d2b30db518f767a457575ea5b25db0adc2ee
Adan Escamilla <adanescamilla580@gmail.com>
2021-08-01 12:48:38 -0700

    fix

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Rating from '../components/Rating'
-import data from '../data';
+import React from "react";
+import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
+import data from "../data";
 
 export default function ProductScreen(props) {
   const product = data.products.find((x) => x._id === props.match.params.id);
@@ -13,7 +13,7 @@ export default function ProductScreen(props) {
       <Link to="/">Back to result</Link>
       <div className="row top">
         <div className="col-2">
-          <img className="large" src={product.image} alt={product.name}/>
+          <img className="large" src={product.image} alt={product.name} />
         </div>
         <div className="col-1">
           <ul>
