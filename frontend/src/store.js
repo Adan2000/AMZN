a77aef83f142624e99da9535139d43afd1208739
Adan Escamilla <adanescamilla580@gmail.com>
2021-08-18 23:00:16 -0700

    redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,11 @@
 import data from './data'
+import { createStore } from 'redux'
+
 const initialState = {}
 const reducer = (state, action) => {
     return {products: data.products}
 }
 
-const store = createStore()
+const store = createStore(reducer, initialState);
+
+export default store;
