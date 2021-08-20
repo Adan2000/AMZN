bc6ad2fd2c2f0ccee4ba203e88892f0fd15d23f5
Adan Escamilla <adanescamilla580@gmail.com>
2021-08-19 18:46:57 -0700

    action

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,7 @@
+import { PRODUCTS_LIST_REQUEST } from "../constants/productConstants"
+
+export const listProducst = () => async (dispatch) => {
+    dispatch({
+        type: PRODUCTS_LIST_REQUEST
+    })
+}
