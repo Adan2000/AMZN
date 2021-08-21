31cbf8ae43e6a6cd2315185cbe8380d01b0a9bea
Adan Escamilla <adanescamilla580@gmail.com>
2021-08-20 21:11:08 -0700

    action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,11 +1,12 @@
 import { PRODUCTS_LIST_REQUEST } from "../constants/productConstants"
+import Axios from 'axios'
 
 export const listProducst = () => async (dispatch) => {
     dispatch({
         type: PRODUCTS_LIST_REQUEST
     })
     try{
-        const { data } = await axios.get("/api/products");
+        const { data } = await Axios.get("/api/products");
     } catch(error){
 
     }
