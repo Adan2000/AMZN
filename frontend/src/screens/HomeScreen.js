1e327d1b56ac95cd74c80455ce5133cc73d4aa41
Adan Escamilla <adanescamilla580@gmail.com>
2021-08-22 16:03:53 -0700

    fix

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,26 +1,17 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Product from "../components/Product";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
+import { useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
 export default function HomeScreen() {
 
   const productList = useSelector( (state) => state.productList )
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get("/api/products");
-        setLoading(false);
-        setProducts(data);
-      } catch (error) {
-        setError(error.message);
-        setLoading(false);
-      }
-    };
-    fetchData();
+    dispatch(listProducts())
   }, []);
 
   return (
